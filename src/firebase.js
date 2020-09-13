49e61599ee0a7fcecb4d86e998459f25e48e81bb
faker007 <rlaqjadud195@naver.com>
2020-09-14 00:02:13 +0900

    added firebase funtionality

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCAw8AAOCXHNriImSNGyMw-f77UETi3cXA",
+  authDomain: "nwitter-97158.firebaseapp.com",
+  databaseURL: "https://nwitter-97158.firebaseio.com",
+  projectId: "nwitter-97158",
+  storageBucket: "nwitter-97158.appspot.com",
+  messagingSenderId: "1071543098132",
+  appId: "1:1071543098132:web:b546e1d1f568200f864001",
+  measurementId: "G-MVH91GNDGM",
+};
+
+export default firebase.initializeApp(firebaseConfig);
