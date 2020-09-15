a4a36d93842e0480e1ecf73bbb1629adbe87eecb
faker007 <rlaqjadud195@naver.com>
2020-09-15 21:54:59 +0900

    Working in Day 2

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,16 @@
 import React from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => (
+  <div>
+    <form>
+      <input type="text" placeholder="Email" required />
+      <input type="password" placeholder="Password" required />
+      <input type="submit" value="Log In" />
+    </form>
+    <div>
+      <button>Continue With Google</button>
+      <button>Continue With Github</button>
+    </div>
+  </div>
+);
 export default Auth;
